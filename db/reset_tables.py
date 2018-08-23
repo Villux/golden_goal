393f32c1ed5bcf24e4fd0fec1556c3867d336afe
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-23 17:01:48 +0300

    make sure not run accidentaly

--- db/reset_tables.py
+++ db/reset_tables.py
@@ -5,14 +5,14 @@ from db.interface import execute_statement, open_connection, close_connection
 
 if __name__ == "__main__":
     conn = open_connection()
-    execute_statement(pq.drop_query, conn)
-    execute_statement(pq.create_query, conn)
-    execute_statement(pq.create_index, conn)
+    # execute_statement(pq.drop_query, conn)
+    # execute_statement(pq.create_query, conn)
+    # execute_statement(pq.create_index, conn)
 
-    execute_statement(mq.drop_query, conn)
-    execute_statement(mq.create_query, conn)
-    execute_statement(mq.create_index, conn)
+    # execute_statement(mq.drop_query, conn)
+    # execute_statement(mq.create_query, conn)
+    # execute_statement(mq.create_index, conn)
 
-    execute_statement(q.drop_elo_table, conn)
-    execute_statement(q.create_elo_table, conn)
+    # execute_statement(q.drop_elo_table, conn)
+    # execute_statement(q.create_elo_table, conn)
     close_connection(conn)
