42c6396f49ec2b53262f59f8e5eee905784dc349
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-23 11:45:51 +0300

    correct name for date param

--- db/player_table.py
+++ db/player_table.py
@@ -9,13 +9,13 @@ def insert(conn, **kwargs):
     execute_statement((query, values), conn)
     return execute_statement("select last_insert_rowid()", conn)
 
-def update_by_fifa_id_and_date(fifa_id, date, conn, **kwargs):
+def update_by_fifa_id_and_date(fifa_id, date_id, conn, **kwargs):
     set_text = build_update_query_set(kwargs)
-    query = f"UPDATE player_table SET {set_text} WHERE fifa_id={fifa_id} AND date='{date}';"
+    query = f"UPDATE player_table SET {set_text} WHERE fifa_id={fifa_id} AND date='{date_id}';"
     execute_statement((query, get_value_tuple(kwargs)), conn)
 
-def get_existing_indexes(indexes, date, conn):
+def get_existing_indexes(indexes, date_id, conn):
     id_list = ','.join(str(x) for x in indexes)
-    query = f"select fifa_id from player_table where fifa_id in ({id_list}) and date='{date}';"
+    query = f"select fifa_id from player_table where fifa_id in ({id_list}) and date='{date_id}';"
     existing_idx = fetchall(query, conn)
     return [idx[0] for idx in existing_idx]
--- services/player.py
+++ services/player.py
@@ -7,9 +7,9 @@ def insert_players(df, conn):
         record["fifa_id"] = idx
         pt.insert(conn=conn, **record)
 
-def update_players(df, conn):
+def update_players_by_fifa_id_and_date(df, date_id, conn):
     for idx, record in df.to_dict('index').items():
-        pt.update_by_fifa_id_and_date(idx, record["date"], conn, **record)
+        pt.update_by_fifa_id_and_date(idx, date_id, conn, **record)
 
 def insert_or_update_player_data(player_data, date):
     conn = open_connection()
@@ -20,6 +20,6 @@ def insert_or_update_player_data(player_data, date):
     new_df = player_data.loc[new_idx]
 
     insert_players(new_df, conn)
-    update_players(existing_df, conn)
+    update_players_by_fifa_id_and_date(existing_df, date, conn)
 
     close_connection(conn)
