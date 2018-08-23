ad37d31f9460254a2c2ab074f816473bb8aa89d4
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-23 17:07:47 +0300

    get n previous matches

--- db/match_table.py
+++ db/match_table.py
@@ -16,10 +16,18 @@ def insert(conn, **kwargs):
     execute_statement((query, values), conn)
     return execute_statement("select last_insert_rowid()", conn)
 
-def get_mutual_matches_between_dates(team, start, end, conn):
+def get_matches_between_dates(team, start, end, conn):
     query = f"select * \
         from match_table \
         where Date > '{start}' AND Date < '{end}' AND \
         (HomeTeam='{team}' OR AwayTeam='{team}');"
 
+    return pd.read_sql(query, conn)
+
+def get_n_latest_matches_before_date(team, date, N, conn, hometeam=True):
+    if hometeam:
+        column = "HomeTeam"
+    else:
+        column = "AwayTeam"
+    query = f"select * from match_table where Date < '{date}' AND {column}='{team}' order by Date desc limit {N};"
     return pd.read_sql(query, conn)
