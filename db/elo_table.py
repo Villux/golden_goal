de1d83d68f8acf66d70e45f2ccb8bf258af193cb
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-24 17:11:04 +0300

    new methods

--- db/elo_table.py
+++ db/elo_table.py
@@ -6,11 +6,17 @@ table_name = "elo_table"
 def insert(conn, **kwargs):
     return dbh.insert(table_name, conn, **kwargs)
 
-def select_latest_for_team(team, date, conn):
-    query = 'SELECT elo, id FROM elo_table WHERE team=? AND date < ? ORDER BY date DESC;'
-    return fetchone((query, (team,date)), conn)[0]
+def select_latest_for_team(team, date, season_id, **kwargs):
+    query = 'SELECT elo, id FROM elo_table WHERE team=? AND date < ? AND season_id=? ORDER BY date DESC;'
+    return fetchone((query, (team, date, season_id)), kwargs["conn"])
 
-def attach_match_to_elo(match_id, id_a, id_b, conn):
+def select_latest_for_season(season_id, **kwargs):
+    asc = kwargs.get("asc", True)
+    query = f"SELECT elo, id FROM elo_table WHERE season_id=? ORDER BY elo {'asc' if asc else 'desc'};"
+    return fetchone((query, (season_id,)), kwargs["conn"])
+
+def attach_match_to_elo(match_id, id_a, id_b, **kwargs):
+    conn = kwargs["conn"]
     query = f"update elo_table set match_id=? where id=?;"
     execute_statement((query, (match_id, id_a)), conn)
     execute_statement((query, (match_id, id_b)), conn)
