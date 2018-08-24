3550ffc7b23664a9a9b18e8fdd0bf6b3aaa9f8b3
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-24 22:36:46 +0300

    columns and amounts

--- services/player.py
+++ services/player.py
@@ -1,6 +1,24 @@
 import numpy as np
 from db import player_table as pt
 
+TOP = 3
+HALF =  5
+FIELD =  10
+GK = 2
+ALL = 25
+
+field_list = [("acceleration", TOP), ("age", ALL), ("aggression", TOP), ("agility", TOP),
+              ("balance", TOP), ("ball_control", HALF), ("composure", TOP), ("crossing", HALF),
+              ("curve", HALF), ("dribbling", HALF), ("finishing", TOP), ("fk_accuracy", TOP),
+              ("gk_diving", GK), ("gk_handling", GK), ("gk_kicking", GK), ("gk_positioning", GK),
+              ("gk_reflexes", GK), ("growth", HALF), ("heading_accuracy", HALF), ("height", FIELD),
+              ("interceptions", HALF), ("international_reputation", ALL), ("jumping", TOP),
+              ("long_passing", HALF), ("long_shots", TOP), ("marking", TOP), ("overall_rating", ALL),
+              ("penalties", TOP), ("positioning", HALF), ("potential", ALL), ("reactions", HALF),
+              ("short_passing", HALF), ("shot_power", TOP), ("skill_moves", TOP), ("sliding_tackle", TOP),
+              ("sprint_speed", TOP), ("stamina", FIELD), ("standing_tackle", HALF), ("strength", FIELD),
+              ("vision", HALF), ("volleys", TOP), ("week_foot", HALF), ("weight", ALL)]
+
 def insert_players(df, conn):
     for idx, record in df.to_dict('index').items():
         record["fifa_id"] = idx
