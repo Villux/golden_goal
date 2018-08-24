c336c2d704e5326db2f86975f873210a7d3156c3
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-24 11:04:52 +0300

    general insert method

--- db/helper.py
+++ db/helper.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from db.interface import execute_statement, fetchone
+
 insert_query = 'insert into {table_name} ({columns}) values ({placeholders})'
 
 def guarantee_python_types(obj):
@@ -19,8 +21,12 @@ def build_insert_query(data, table_name):
 
 def build_update_query_set(data):
     elements = []
-
-    for key, value in data.items():
+    for key, _ in data.items():
         elements.append(f'{key}=?')
     return ','.join(elements)
 
+def insert(table_name, conn, **kwargs):
+    query = build_insert_query(kwargs, table_name)
+    values = get_value_tuple(kwargs)
+    execute_statement((query, values), conn)
+    return fetchone("select last_insert_rowid()", conn)[0]
