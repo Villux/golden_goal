8835345bd0bdd3c88f465f07b781264c385a94d8
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-27 19:43:16 +0300

    new methods

--- models/outcome_model.py
+++ models/outcome_model.py
@@ -1,10 +1,16 @@
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
-from services.data_provider import get_dataset
+from services.data_provider import DataLoader, feature_columns
+from db.interface import open_connection, close_connection
+
+def get_default_param():
+    return {'max_depth': 8, 'max_features': 'log2', 'min_samples_leaf': 3, 'n_estimators': 2000, "oob_score": True, "bootstrap": True, "n_jobs": -1}
 
 def run_grid_search(grid):
-    X, y = get_dataset("outcome", path="master_data.csv")
+    conn = open_connection()
+    data_loader = DataLoader(feature_columns, "outcome", conn)
+    X, y = data_loader.get_dataset()
 
     tuning = GridSearchCV(
         estimator=RandomForestClassifier(oob_score=True, bootstrap=True, n_jobs=-1),
@@ -12,7 +18,15 @@ def run_grid_search(grid):
         scoring='accuracy',
         cv=5)
     tuning.fit(X, y)
+
+    close_connection(conn)
     return tuning.best_params_, tuning.best_score_
+
+def get_model(params, X=None, y=None):
+    model = RandomForestClassifier(**params)
+    if (X is not None) and (y is not None):
+        model.fit(X, y)
+    return model
 
 if __name__ == "__main__":
     param_grid = {
@@ -21,5 +35,5 @@ if __name__ == "__main__":
         'max_features': ["sqrt", "log2"],
         'n_estimators': [2000]
     }
-    params, score = run_grid_search(param_grid)
-    print("Best params", params, score)
+    best_params, score = run_grid_search(param_grid)
+    print("Best params", best_params, score)
