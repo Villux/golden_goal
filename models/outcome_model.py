28265fdf2830fcb37aba023c887e744077dc0810
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-27 20:36:42 +0300

    fix data loader bug

--- models/outcome_model.py
+++ models/outcome_model.py
@@ -2,14 +2,12 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
 from services.data_provider import DataLoader, feature_columns
-from db.interface import open_connection, close_connection
 
 def get_default_param():
-    return {'max_depth': 8, 'max_features': 'log2', 'min_samples_leaf': 3, 'n_estimators': 2000, "oob_score": True, "bootstrap": True, "n_jobs": -1}
+    return {'max_depth': 8, 'max_features': 'log2', 'min_samples_leaf': 3,
+            'n_estimators': 2000, "oob_score": True, "bootstrap": True, "n_jobs": -1}
 
-def run_grid_search(grid):
-    conn = open_connection()
-    data_loader = DataLoader(feature_columns, "outcome", conn)
+def run_grid_search(grid, data_loader):
     X, y = data_loader.get_dataset()
 
     tuning = GridSearchCV(
@@ -19,7 +17,6 @@ def run_grid_search(grid):
         cv=5)
     tuning.fit(X, y)
 
-    close_connection(conn)
     return tuning.best_params_, tuning.best_score_
 
 def get_model(params, X=None, y=None):
@@ -35,5 +32,6 @@ if __name__ == "__main__":
         'max_features': ["sqrt", "log2"],
         'n_estimators': [2000]
     }
-    best_params, score = run_grid_search(param_grid)
+    dl = DataLoader(feature_columns, "outcome")
+    best_params, score = run_grid_search(param_grid, dl)
     print("Best params", best_params, score)
