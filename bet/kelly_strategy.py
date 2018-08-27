ffcb3d70b5a9d7304aa67340968fb951c305ed9d
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-27 19:42:32 +0300

    betting strategies

---
+++ bet/kelly_strategy.py
@@ -0,0 +1,80 @@
+import numpy as np
+import scipy.optimize as optimize
+
+from bet.strategy import Strategy
+
+def kelly_function(params, odds, probabilities):
+    a, b, c = params
+    o1, o2, o3 = odds
+    p1, p2, p3 = probabilities
+    return -(p1 * np.log(1 + o1*a - b - c) + p2 * np.log(1 + o2*b - a - c) + p3*np.log(1 + o3*c - a - b))
+
+def kelly_function_deriv(params, odds, probabilities):
+    a, b, c = params
+    o1, o2, o3 = odds
+    p1, p2, p3 = probabilities
+    dfda = -(p1 * o1)/(o1*a-b-c+1) + p2/(o2*b-a-c+1) + p3/(o3*c-b-a+1)
+    dfdb = p1/(o1*a-b-c+1) - (p2 * o2)/(o2*b-a-c+1) + p3/(o3*c-b-a+1)
+    dfdc = p1/(o1*a-b-c+1) + p2/(o2*b-a-c+1) - (p3*o3)/(o3*c-b-a+1)
+    return np.array([dfda, dfdb, dfdc])
+
+def constraint(x):
+    return 1.00000001 - (x[0]+x[1]+x[2])
+
+class KellyStrategy(Strategy):
+    def __init__(self, outcomes, initial_capital=64):
+        super().__init__(initial_capital=initial_capital)
+        self.outcomes = outcomes
+        self.max_iterations = 20
+
+        self.opf = []
+
+    def get_optimal_fractions(self, odds, probabilities):
+        args = (odds, probabilities)
+        bounds = ((0.0, 1.0), (0.0, 1.0), (0.0, 1.0))
+        cons = {'type': 'ineq', 'fun': constraint}
+        for i in range(self.max_iterations):
+            initial_guess = [0.05, 0.05, 0.05]
+            result = optimize.minimize(
+                kelly_function,
+                initial_guess,
+                bounds=bounds,
+                constraints=cons,
+                args=args,
+                jac=kelly_function_deriv
+            )
+
+            if result.success:
+                break
+            else:
+                initial_guess = np.random.uniform(low=0.005, high=0.1, size=3)
+            if i + 1 == self.max_iterations:
+                return [0.0, 0.0, 0.0]
+
+        fs = result.x
+        for i, f in enumerate(fs):
+            if f < 0.0001:
+                fs[i] = 0
+        return fs
+
+    def get_odds_probabilities_and_fractions(self):
+        return self.opf
+
+    def run(self, odds, probabilities, coef=0.3):
+        for _, (match_odds, match_probabilities, outcome) in enumerate(zip(odds, probabilities, self.outcomes)):
+            net_match_odds = match_odds - 1
+            bet_fractions = self.get_optimal_fractions(net_match_odds, match_probabilities)
+
+            self.opf.append(list(zip(match_odds, match_probabilities, bet_fractions)))
+
+            cost = 0
+            returns = 0
+            for k, (odd, fraction) in enumerate(zip(match_odds, bet_fractions)):
+                bet_size = self.balance * fraction * coef
+                if bet_size < 0.1:
+                    bet_size = 0
+                cost += bet_size
+                if outcome == 1 - k:
+                    returns += bet_size * odd
+            self.update_balance(returns - cost)
+            self.store_cost(cost)
---
+++ bet/single_kelly_strategy.py
@@ -0,0 +1,48 @@
+import numpy as np
+from bet.strategy import Strategy
+
+class KellyStrategySingle(Strategy):
+    def __init__(self, outcomes, predicted_outcomes, initial_capital=64):
+        super().__init__(initial_capital=initial_capital)
+        self.outcomes = outcomes
+        self.predicted_outcomes = predicted_outcomes
+
+    @staticmethod
+    def calculate_optimal_fraction(b, p):
+        return (p*(b+1) -1)/b
+
+    def get_optimal_fractions(self, odds, probabilities):
+        fractions = np.zeros(3)
+        for idx, (b, p) in enumerate(zip(odds, probabilities)):
+            fractions[idx] = self.calculate_optimal_fraction(b, p)
+
+        return fractions
+
+    def run(self, odds, probabilities, coef=0.3):
+        for _, (match_odds, match_probabilities, outcome, outcome_pred) in enumerate(zip(odds, probabilities, self.outcomes, self.predicted_outcomes)):
+            net_match_odds = match_odds - 1
+            bet_fractions = self.get_optimal_fractions(net_match_odds, match_probabilities)
+
+            if outcome_pred == 1:
+                odd_idx = 0
+                f = bet_fractions[odd_idx]
+            elif outcome_pred == 0:
+                odd_idx = 1
+                f = bet_fractions[odd_idx]
+            elif outcome_pred == -1:
+                odd_idx = 2
+                f = bet_fractions[odd_idx]
+
+            if f < 0:
+                f = 0
+
+            bet_size = self.balance * f * coef
+
+            if bet_size < 0.1:
+                bet_size = 0
+
+            returns = 0
+            if outcome_pred == outcome:
+                returns = bet_size * match_odds[odd_idx]
+            self.update_balance(returns - bet_size)
+            self.store_cost(bet_size)
