0bcce6235d8542715987549d965fe4b5259df687
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-27 13:36:48 +0300

    run all local

--- db/reset_tables.py
+++ db/reset_tables.py
@@ -8,13 +8,15 @@ if __name__ == "__main__":
     # execute_statement(pq.drop_query, conn)
     # execute_statement(pq.create_query, conn)
     # execute_statement(pq.create_index, conn)
+    # execute_statement(pq.create_date_index, conn)
+    # execute_statement(pq.create_club_index, conn)
 
-    execute_statement(mq.drop_query, conn)
-    execute_statement(mq.create_query, conn)
-    execute_statement(mq.create_index, conn)
+    # execute_statement(mq.drop_query, conn)
+    # execute_statement(mq.create_query, conn)
+    # execute_statement(mq.create_index, conn)
 
-    execute_statement(q.drop_odds_table, conn)
-    execute_statement(q.create_odds_table, conn)
+    # execute_statement(q.drop_odds_table, conn)
+    # execute_statement(q.create_odds_table, conn)
 
     execute_statement(q.drop_division_table, conn)
     execute_statement(q.create_division_table, conn)
@@ -27,5 +29,7 @@ if __name__ == "__main__":
 
     execute_statement(pq.drop_team_feature_query, conn)
     execute_statement(pq.create_team_feature_table, conn)
+    execute_statement(pq.create_team_feature_date_index, conn)
+    execute_statement(pq.create_team_feature_club_index, conn)
 
     close_connection(conn)
