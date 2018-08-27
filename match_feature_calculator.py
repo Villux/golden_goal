19ce2f20f3476aa8e13d6655d0532481bf5112f4
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-27 13:36:05 +0300

    combine all features

--- match_feature_calculator.py
+++ match_feature_calculator.py
@@ -1,21 +1,54 @@
-from services import match, elo
+import argparse
+from multiprocessing import Pool, cpu_count
+import pandas as pd
+
+from services import match, elo, player
 from db.interface import open_connection, close_connection
 from db import match_table as mt
+from db import season_table as st
 
-def calculate_elo_for_matches():
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', type=int)
+parser.add_argument('-f', type=str)
+args = parser.parse_args()
+
+def get_match_datapoints(match_obj):
     conn = open_connection()
-    matches = mt.get_matches(conn)
-    for _, record in matches.to_dict('index').items():
-        home_elo, home_elo_id = elo.get_elo_and_id(record["HomeTeam"], record["Date"], record["season_id"], conn=conn)
-        away_elo, away_elo_id = elo.get_elo_and_id(record["AwayTeam"], record["Date"], record["season_id"], conn=conn)
-        elo.attach_elo_to_match(record["id"], home_elo_id, away_elo_id, conn=conn)
+    date = match_obj["Date"]
+    season_id = match_obj["season_id"]
+    N = 20
 
-        new_home_elo, new_away_elo = elo.get_elo_from_result(home_elo, away_elo, record["FTHG"], record["FTAG"])
-        elo.insert(record["HomeTeam"], record["season_id"], record["Date"], new_home_elo, conn=conn)
-        elo.insert(record["AwayTeam"], record["season_id"], record["Date"], new_away_elo, conn=conn)
-        conn.commit()
+    home_team = match_obj["HomeTeam"]
+    match_obj["home_xg"] = match.calculate_xg(home_team, date, N, combination=match.HOMEAWAY, conn=conn)
+    match_obj["home_goal_mean"] = match.calculate_goal_average(home_team, date, N, conn=conn)
+    match_obj["home_elo"] = elo.get_elo_and_id(home_team, date, season_id, conn=conn)
+
+    away_team = match_obj["AwayTeam"]
+    match_obj["away_xg"] = match.calculate_xg(away_team, date, N, combination=match.HOMEAWAY, conn=conn)
+    match_obj["away_goal_mean"] = match.calculate_goal_average(away_team, date, N, conn=conn)
+    match_obj["away_elo"] = elo.get_elo_and_id(away_team, date, season_id, conn=conn)
+
+    home, away = player.get_team_features_for_matches(home_team, away_team, date, conn=conn)
 
     close_connection(conn)
+    return {**match, **home, **away}
+
+def get_data_for_matches(matches):
+    func_args = [record for _, record in matches.to_dict('index').items()]
+    pool = Pool(cpu_count())
+    data_list = pool.map(get_match_datapoints, func_args)
+    return pd.DataFrame(data_list)
 
 if __name__ == "__main__":
-    calculate_elo_for_matches()
+    connection = open_connection()
+    if args.d:
+        season_tuples = st.get_seasons_for_division(args.d)
+        season_ids = [season[0] for season in season_tuples]
+    else:
+        season_ids = None
+
+    all_matches = mt.get_matches_for_seasons(season_ids, conn=connection)
+    close_connection(connection)
+
+    results = get_data_for_matches(all_matches)
+    results.to_csv(args.f)
