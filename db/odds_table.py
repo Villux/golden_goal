4a371a0e56930c6262f6e9ae801a6b4a4ed7dab0
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-27 19:42:49 +0300

    get odds for match

--- db/odds_table.py
+++ db/odds_table.py
@@ -1,6 +1,11 @@
 from db import helper as dbh
+from db.interface import fetchone
 
 table_name = 'odds_table'
 
 def insert(conn, **kwargs):
     return dbh.insert(table_name, conn, **kwargs)
+
+def get_for_match(match_id, description, **kwargs):
+    query = f"select home_win, draw, away_win from odds_table where match_id={match_id} and description='{description}'"
+    return fetchone(query, kwargs["conn"])
