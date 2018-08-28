c11b68111bf659a375b3379d92befe37b1502ba6
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-28 11:25:11 +0300

    plot methods

--- simulation/utils.py
+++ simulation/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def plot_bank_and_bets(strategy):
+def plot_strategy(strategy, prefix):
     initial_capital = strategy.initial_capital
     net_returns = np.array(strategy.get_returns())
     returns = net_returns + 1
@@ -8,22 +9,21 @@ def plot_bank_and_bets(strategy):
     costs = np.array(strategy.costs) * -1
 
     balance_progression = np.cumprod(returns)
-    bar_labels = [1 if value > 0 else 0 for value in net_returns]
-
     net_flows = balance_progression - np.insert(balance_progression, 0, initial_capital)[:-1]
     winnnings = np.array([value if value > 0 else 0 for value in net_flows])
 
     figsize = (12, 6)
-    colors = {0: 'r', 1: 'g'}
-    fig, ax = plt.subplots(figsize=figsize)
+    plt.subplots(figsize=figsize)
     index = np.arange(len(costs))
-    ax.bar(index, winnnings, color='g')
-    ax.bar(index, costs, color='r')
-
-    ax.set_xticks(np.arange(0, len(net_returns)))
-    plt.xticks(rotation='vertical')
+    plt.bar(index, winnnings, color='g')
+    plt.bar(index, costs, color='r')
+    plt.savefig(f"img/{prefix}_pl.png")
 
     plt.subplots(figsize=figsize)
-    plt.xticks(np.arange(0, len(net_returns)))
-    plt.xticks(rotation='vertical')
     plt.plot(balance_progression)
+    plt.savefig(f"img/{prefix}_progression.png")
+
+def plot_simulation(simulation):
+    plot_strategy(simulation.unit_strategy, f"unit_season_{simulation.season_id}")
+    plot_strategy(simulation.kelly_strategy, f"kelly_season_{simulation.season_id}")
+    plot_strategy(simulation.kelly_strategy_single, f"kelly_single_season_{simulation.season_id}")
