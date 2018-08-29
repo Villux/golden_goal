f1ddb1f0a593bccba8b1005f06cf85e5cdbe85e3
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-08-29 09:52:34 +0300

    model improved

--- models/lr_model.py
+++ models/lr_model.py
@@ -5,13 +5,13 @@ from sklearn.linear_model import LogisticRegression
 from services.data_provider import DataLoader, feature_columns
 
 def get_default_param():
-    return {'n_jobs': cpu_count(), "solver": "newton-cg", 'C': 0.001, 'penalty': 'l2'}
+    return {"solver": "newton-cg", 'C': 1e-06, 'penalty': 'l2'}
 
 def run_grid_search(grid, data_loader):
     X, y = data_loader.get_dataset()
 
     tuning = GridSearchCV(
-        estimator=LogisticRegression(n_jobs=cpu_count(), solver="newton-cg"),
+        estimator=LogisticRegression(n_jobs=cpu_count(), solver="newton-cg", max_iter=500),
         param_grid=grid,
         scoring='accuracy',
         cv=5)
@@ -37,7 +37,7 @@ def get_model(params, X=None, y=None):
 if __name__ == "__main__":
     param_grid = {
         'penalty': ['l2'],
-        'C': [0.001,0.01,0.1,1,10,100,1000]
+        'C': [0.0000001, 0.000001, 0.000005, 0.00001, 0.0005, 0.0001]
     }
 
     dl = DataLoader(feature_columns, "outcome")
