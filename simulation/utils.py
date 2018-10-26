9ace773df5d3fddda9ec868add4b10ea572e3cfe
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-10-26 10:28:07 +0300

    plot prob

--- simulation/utils.py
+++ simulation/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from utils import write_as_json
 
 def plot_strategy(strategy, prefix):
     initial_capital = strategy.initial_capital
@@ -27,3 +28,28 @@ def plot_simulation(simulation):
     plot_strategy(simulation.unit_strategy, f"unit_season_{simulation.season_id}")
     plot_strategy(simulation.kelly_strategy, f"kelly_season_{simulation.season_id}")
     plot_strategy(simulation.kelly_strategy_single, f"kelly_single_season_{simulation.season_id}")
+
+def plot_probabilities_against_market(simulation_probabilities, market_probabilities, filename):
+    for k, label in enumerate(["home", "draw", "away"]):
+        simu_prob = [prob[k] for prob in simulation_probabilities]
+        market_prob = [prob[k] for prob in market_probabilities]
+
+        figsize = (12, 6)
+        plt.subplots(figsize=figsize)
+        plt.plot(simu_prob, label="Simulation", alpha=0.7)
+        plt.plot(market_prob, label="Market", alpha=0.7)
+        plt.legend()
+        plt.ylim(0, 1)
+        plt.ylabel('Probability')
+        plt.xlabel('Match')
+        plt.savefig(f'img/{label}_{filename}.svg')
+
+        data = {
+            "simulation_avg": np.mean(simu_prob),
+            "simulation_std": np.std(simu_prob),
+            "market_avg": np.mean(market_prob),
+            "market_std": np.std(market_prob),
+            "corr": np.corrcoef(simu_prob, market_prob).tolist()
+        }
+
+        write_as_json(data, f"img/meta/{label}_{filename}_probability_comparison")
