a89fb713a200e86f5d01c499a6ef03372cfc7038
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-10-29 20:08:21 +0200

    table queries

--- db/queries.py
+++ db/queries.py
@@ -38,3 +38,56 @@ create_season_table = '''CREATE TABLE season_table
                     FOREIGN KEY(division_id) REFERENCES division_table(id));'''
 
 drop_season_table = "DROP TABLE IF EXISTS season_table;"
+
+create_lineup_table = '''CREATE TABLE lineup_table
+                    (id integer PRIMARY KEY AUTOINCREMENT,
+                    url text,
+                    p1 integer,
+                    p2 integer,
+                    p3 integer,
+                    p4 integer,
+                    p5 integer,
+                    p6 integer,
+                    p7 integer,
+                    p8 integer,
+                    p9 integer,
+                    p10 integer,
+                    p11 integer,
+                    s1 integer,
+                    s2 integer,
+                    s3 integer,
+                    s4 integer,
+                    s5 integer,
+                    s6 integer,
+                    s7 integer,
+                    match_id integer,
+                    FOREIGN KEY(p1) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p2) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p3) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p4) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p5) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p6) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p7) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p8) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p9) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p10) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(p11) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s1) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s2) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s3) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s4) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s5) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s6) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(s7) REFERENCES player_identity_table(id),
+                    FOREIGN KEY(match_id) REFERENCES match_table(id));'''
+
+drop_lineup_table = "DROP TABLE IF EXISTS lineup_table;"
+
+create_player_identity_table = '''CREATE TABLE player_identity_table
+                    (id integer PRIMARY KEY AUTOINCREMENT,
+                    fifa_name text,
+                    goalcom_name text,
+                    fifa_id INTEGER,
+                    goalcom_url text);'''
+
+drop_player_identity_table = "DROP TABLE IF EXISTS player_identity_table;"
