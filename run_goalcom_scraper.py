d91787155d43f17c3eceb1fee50496cd62bed7e8
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-10-29 16:31:21 +0200

    date filter arg

--- run_goalcom_scraper.py
+++ run_goalcom_scraper.py
@@ -3,9 +3,19 @@ import argparse
 import pandas as pd
 import goalcom.scraper as goalcom
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', type=str)
+args = parser.parse_args()
+
 def read_dates(filename="match_dates.csv"):
     date_df = pd.read_csv(filename)
     return date_df.Date.values
 
 dates = read_dates()
-goalcom.run(dates)
+
+query_dates = []
+for date in dates:
+    if time.strptime(date, "%Y-%m-%d") >= time.strptime(args.d, "%Y-%m-%d"):
+        query_dates.append(date)
+
+goalcom.run(query_dates)
