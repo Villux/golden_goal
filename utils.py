144b687f64c65565791cdb159ae565c88ef69590
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-10-30 16:12:04 +0200

    ascii and extra keys methods

--- utils.py
+++ utils.py
@@ -1,5 +1,17 @@
 import json
+import unicodedata
 
 def write_as_json(data, filename):
     with open(f'{filename}.json', 'w') as outfile:
         json.dump(data, outfile, indent=4)
+
+def remove_extra_keys(record, valid_keys):
+    new_dict = {}
+    for valid_key in valid_keys:
+        value = record.get(valid_key, None)
+        if value:
+            new_dict[valid_key] = value
+    return new_dict
+
+def unicode_to_ascii(value):
+    return unicodedata.normalize('NFD', value).encode('ascii', 'ignore').decode('ascii')
