a911169dc196487138ee313cc7dea9c47413d0a5
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-10-30 16:12:15 +0200

    new tables

--- db/lineup_table.py
+++ db/lineup_table.py
@@ -1,8 +1,30 @@
 import pandas as pd
-from db.interface import execute_statement
+from db.interface import execute_statement, fetchone
 from db import helper as dbh
 
 table_name = 'lineup_table'
 
 def insert(conn, **kwargs):
     return dbh.insert(table_name, conn, **kwargs)
+
+def build_lineup_object(player_sets, match_id):
+    data = {}
+    for ps_idx, player_set_prefix in enumerate(["hp", "hs", "ap", "as"]):
+        for idx, player_id in enumerate(player_sets[ps_idx]):
+            data[f"{player_set_prefix}{idx+1}"] = player_id
+    data["match_id"] = match_id
+    return data
+
+def insert_player_for_match(player_sets, match_id, **kwargs):
+    data = build_lineup_object(player_sets, match_id)
+    insert(kwargs["conn"], **data)
+
+def get_by_match_id(match_id, **kwargs):
+    query = f"select * from lineup_table where match_id={match_id}"
+    return fetchone(query, kwargs["conn"])
+
+def update(match_id, player_sets, conn):
+    data = build_lineup_object(player_sets, match_id)
+    set_text = dbh.build_update_query_set(data)
+    query = f"UPDATE lineup_table SET {set_text} WHERE match_id={match_id};"
+    execute_statement((query, dbh.get_value_tuple(data)), conn)
--- db/player_identity_table.py
+++ db/player_identity_table.py
@@ -4,5 +4,16 @@ from db import helper as dbh
 
 table_name = 'player_identity_table'
 
+VALID_KEYS = ["id", "fifa_id", "fifa_name", "goalcom_url", "goalcom_name"]
+
 def insert(conn, **kwargs):
     return dbh.insert(table_name, conn, **kwargs)
+
+def update_by_fifa_id(fifa_id, conn, **kwargs):
+    set_text = dbh.build_update_query_set(kwargs)
+    query = f"UPDATE player_identity_table SET {set_text} WHERE fifa_id={fifa_id};"
+    execute_statement((query, dbh.get_value_tuple(kwargs)), conn)
+
+def get_by_fifa_id(fifa_id, **kwargs):
+    query = f"SELECT * from player_identity_table WHERE fifa_id={fifa_id};"
+    return pd.read_sql(query, kwargs["conn"])
